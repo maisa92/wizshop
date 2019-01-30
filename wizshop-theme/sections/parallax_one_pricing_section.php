78c257bbc5a418218e1b1c255645ffa1c6d851ab
Saba Pruidze <sabaprui@gmail.com>
2019-01-30 20:50:04 +0400

    change pricing table

--- wizshop-theme/sections/parallax_one_pricing_section.php
+++ wizshop-theme/sections/parallax_one_pricing_section.php
@@ -4,73 +4,78 @@
  *
  * @package parallax-one
  */
-	?>
+?>
 
-	<section class="wiz-pricing" id="pricing" ">
+    <section class="wiz-pricing" id="pricing" ">
 
-		<div class="section-overlay-layer">
-			<div class="container">
-				<div class="row">
-					<!-- DIRTY Responsive pricing table HTML -->
-					<h2 class="dark-text">ძნელია მაგრამ უნდა დაიჯეროთ</h2><div class="colored-line"></div>
-					<br>
-					<br>
-					<article>
-						<table>
-							<thead>
-							<tr>
-								<th class=""></th>
-								<th class="bg-purple">სტანდართული</th>
-								<th class="bg-blue">პრემიუმი</th>
-							</tr>
-							</thead>
-							<tbody>
-							<tr>
-								<td><strong>ყოველთიური გადასახადი</strong></td>
-								<td><span class="txt-top"></span><span class="txt-l">60 ლარი</span></td>
-								<td><span class="txt-top"></span><span class="txt-l">ინდივიდუალური</span></td>
-							</tr>
+    <div class="section-overlay-layer">
+        <div class="container">
+            <div class="row">
+                <!-- DIRTY Responsive pricing table HTML -->
+                <h2 class="dark-text">ძნელია მაგრამ უნდა დაიჯეროთ</h2><div class="colored-line"></div>
+                <br>
+                <br>
+                <article>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th class=""></th>
+                            <th class="bg-purple">სტანდართული</th>
+                            <th class="bg-blue">პრემიუმი</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td><strong>ყოველთიური გადასახადი</strong></td>
+                            <td><span class="txt-top"></span><span class="txt-l">60 ლარი</span></td>
+                            <td><span class="txt-top"></span><span class="txt-l">ინდივიდუალური</span></td>
+                        </tr>
 
-							<tr>
-								<td>სრულიად ფუნქციონირებადი ონლაინ მაღაზია</td>
-								<td><span class="tick">&#10004;</span></td>
-								<td><span class="tick">&#10004;</span></td>
-							</tr>
-                            <tr>
-                                <td>SSL სერთიფიკატი</td>
-                                <td><span class="tick">&#10004;</span></td>
-                                <td><span class="tick">&#10004;</span></td>
-                            </tr>
-							<tr>
-								<td><a href="#customers">Without</a> საგადამხდელო სისტემa</td>
-								<td><span class="tick">&#10004;</span></td>
-								<td><span class="tick">&#10004;</span></td>
-							</tr>
-							<tr>
-								<td>თქვენ მიერ მოტანილი საგადამხდელო სისტემა</td>
-								<td><span class="tick">&#10004;</span></td>
-								<td><span class="tick">&#10004;</span></td>
-							</tr>
-							<tr>
-								<td>არჩევანი ვებსაიტის დიზაინში</td>
-								<td></td>
-								<td><span class="tick">&#10004;</span></td>
-							</tr>
-							<tr>
-								<td>Support</td>
-								<td></td>
-								<td><span class="tick">&#10004;</span></td>
-							</tr>
-							</tbody>
-						</table>
+                        <tr>
+                            <td>სრულიად ფუნქციონირებადი ონლაინ მაღაზია</td>
+                            <td><span class="tick">&#10004;</span></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td>SSL სერთიფიკატი</td>
+                            <td><span class="tick">&#10004;</span></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td>Cloud ინფრასტრუქტურა</td>
+                            <td><span class="tick">&#10004;</span></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td><a href="#customers">Without</a> საგადამხდელო სისტემა</td>
+                            <td><span class="tick">&#10004;</span></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td>თქვენ მიერ მოტანილი საგადამხდელო სისტემა</td>
+                            <td><span class="tick">&#10004;</span></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td>არჩევანი ვებსაიტის დიზაინში</td>
+                            <td></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        <tr>
+                            <td>Support</td>
+                            <td></td>
+                            <td><span class="tick">&#10004;</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
 
-					</article>
-				</div><!-- .brief-content-one-->
-			</div>
-		</div>
-		</div>
-		<?php parallax_hook_about_bottom(); ?>
-	</section><!-- .brief-design-one -->
-	<?php parallax_hook_about_after(); ?>
-	<?php
+                </article>
+            </div><!-- .brief-content-one-->
+        </div>
+    </div>
+    </div>
+<?php parallax_hook_about_bottom(); ?>
+    </section><!-- .brief-design-one -->
+<?php parallax_hook_about_after(); ?>
+<?php
 
